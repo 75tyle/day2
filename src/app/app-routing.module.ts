704ff509f8a41807e5353e78d6b675c2a596251f
sanghpal <kamblesanghpal2003@gmail.com>
2023-05-05 22:52:04 +0530

    loginform-3

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,10 @@ import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   
-    { path: '', loadChildren: () => import('./registration/registration.module').then(m => m.RegistrationModule) },
- 
+    { path: 'registration', loadChildren: () => import('./registration/registration.module').then(m => m.RegistrationModule) },
+    { path: 'card', loadChildren: () => import('./card/card.module').then(m => m.CardModule) },
+    { path: '', loadChildren: () => import('./slider/slider.module').then(m => m.SliderModule) },
 ];
-
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
